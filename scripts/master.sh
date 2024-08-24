e21c35a6a1930f943eff46e1186ae1072b9ae203
amir001 <amir001.ab@gmail.com>
2024-08-24 18:58:39 +0330

    add kubeadm cri

--- scripts/master.sh
+++ scripts/master.sh
@@ -4,32 +4,31 @@
 
 set -euxo pipefail
 
-# If you need public access to API server using the server's Public IP address, change PUBLIC_IP_ACCESS to true.
+# If you need public access to API server using the servers Public IP adress, change PUBLIC_IP_ACCESS to true.
 
-PUBLIC_IP_ACCESS="true"
+PUBLIC_IP_ACCESS="false"
 NODENAME=$(hostname -s)
 POD_CIDR="192.168.0.0/16"
 
 # Pull required images
 
-sudo kubeadm config images pull
-
+#sudo kubeadm config images pull
+sudo kubeadm config images pull --cri-socket unix:///var/run/crio/crio.sock
 # Initialize kubeadm based on PUBLIC_IP_ACCESS
 
 if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
     
-    MASTER_PRIVATE_IP=$(ip addr show ens3 | awk '/inet / {print $2}' | cut -d/ -f1)
-    sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
+    MASTER_PRIVATE_IP=$(ip addr show enX0 | awk '/inet / {print $2}' | cut -d/ -f1)
+    sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap --cri-socket unix:///var/run/crio/crio.sock
 
 elif [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
-
-    # Replace the below value with your actual public IP
+    
     MASTER_PUBLIC_IP="87.236.166.178"
     MASTER_PRIVATE_IP=$(ip addr show ens3 | awk '/inet / {print $2}' | cut -d/ -f1)
-    sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP,$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
+    sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP,$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --cri-socket unix:///var/run/crio/crio.sock --ignore-preflight-errors Swap
 
 else
-    echo "Error: PUBLIC_IP_ACCESS has an invalid value: $PUBLIC_IP_ACCESS"
+    echo "Error: MASTER_PUBLIC_IP has an invalid value: $PUBLIC_IP_ACCESS"
     exit 1
 fi
 
@@ -39,6 +38,6 @@ mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
-# Install Calico Network Plugin
+# Install Claico Network Plugin Network 
 
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
