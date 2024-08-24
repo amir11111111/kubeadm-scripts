5d96c648f7d4112cd848baee1a39bda36571d21c
amir001 <amir001.ab@gmail.com>
2024-08-24 17:13:44 +0330

    change master script

--- scripts/master.sh
+++ scripts/master.sh
@@ -4,9 +4,9 @@
 
 set -euxo pipefail
 
-# If you need public access to API server using the servers Public IP adress, change PUBLIC_IP_ACCESS to true.
+# If you need public access to API server using the server's Public IP address, change PUBLIC_IP_ACCESS to true.
 
-PUBLIC_IP_ACCESS="false"
+PUBLIC_IP_ACCESS="true"
 NODENAME=$(hostname -s)
 POD_CIDR="192.168.0.0/16"
 
@@ -18,16 +18,18 @@ sudo kubeadm config images pull
 
 if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
     
-    MASTER_PRIVATE_IP=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f1)
+    MASTER_PRIVATE_IP=$(ip addr show ens3 | awk '/inet / {print $2}' | cut -d/ -f1)
     sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
 
 elif [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
 
-    MASTER_PUBLIC_IP=$(curl ifconfig.me && echo "")
-    sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
+    # Replace the below value with your actual public IP
+    MASTER_PUBLIC_IP="87.236.166.178"
+    MASTER_PRIVATE_IP=$(ip addr show ens3 | awk '/inet / {print $2}' | cut -d/ -f1)
+    sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP,$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
 
 else
-    echo "Error: MASTER_PUBLIC_IP has an invalid value: $PUBLIC_IP_ACCESS"
+    echo "Error: PUBLIC_IP_ACCESS has an invalid value: $PUBLIC_IP_ACCESS"
     exit 1
 fi
 
@@ -37,6 +39,6 @@ mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
-# Install Claico Network Plugin Network 
+# Install Calico Network Plugin
 
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
